edfa278ae47fd519050dac7451d5906d548edad5
Vardhaman <vardhaman.surana@suse.com>
2025-09-24 15:55:43 +0530

    container service apis (#2)
    * container service apis
    
    * removed underscores from variable names
    
    * updated comments as per godoc

---
+++ cs/client/client.go
@@ -0,0 +1,87 @@
+package client
+
+import (
+	openapi "github.com/rancher/muchang/darabonba-openapi/client"
+	openapiutil "github.com/rancher/muchang/darabonba-openapi/utils"
+	"github.com/rancher/muchang/utils/tea/dara"
+)
+
+type Client struct {
+	openapi.Client
+	DisableSDKError *bool
+}
+
+func NewClient(config *openapiutil.Config) (*Client, error) {
+	client := new(Client)
+	err := client.Init(config)
+	return client, err
+}
+
+func (client *Client) Init(config *openapiutil.Config) (err error) {
+	err = client.Client.Init(config)
+	if err != nil {
+		return err
+	}
+	client.EndpointRule = dara.String("regional")
+	client.EndpointMap = map[string]*string{
+		"ap-northeast-2-pop":          dara.String("cs.aliyuncs.com"),
+		"cn-beijing-finance-pop":      dara.String("cs.aliyuncs.com"),
+		"cn-beijing-gov-1":            dara.String("cs.aliyuncs.com"),
+		"cn-beijing-nu16-b01":         dara.String("cs.aliyuncs.com"),
+		"cn-edge-1":                   dara.String("cs.aliyuncs.com"),
+		"cn-fujian":                   dara.String("cs.aliyuncs.com"),
+		"cn-haidian-cm12-c01":         dara.String("cs.aliyuncs.com"),
+		"cn-hangzhou-bj-b01":          dara.String("cs.aliyuncs.com"),
+		"cn-hangzhou-internal-prod-1": dara.String("cs.aliyuncs.com"),
+		"cn-hangzhou-internal-test-1": dara.String("cs.aliyuncs.com"),
+		"cn-hangzhou-internal-test-2": dara.String("cs.aliyuncs.com"),
+		"cn-hangzhou-internal-test-3": dara.String("cs.aliyuncs.com"),
+		"cn-hangzhou-test-306":        dara.String("cs.aliyuncs.com"),
+		"cn-hongkong-finance-pop":     dara.String("cs.aliyuncs.com"),
+		"cn-qingdao-nebula":           dara.String("cs.aliyuncs.com"),
+		"cn-shanghai-et15-b01":        dara.String("cs.aliyuncs.com"),
+		"cn-shanghai-et2-b01":         dara.String("cs.aliyuncs.com"),
+		"cn-shanghai-inner":           dara.String("cs.aliyuncs.com"),
+		"cn-shanghai-internal-test-1": dara.String("cs.aliyuncs.com"),
+		"cn-shenzhen-inner":           dara.String("cs.aliyuncs.com"),
+		"cn-shenzhen-st4-d01":         dara.String("cs.aliyuncs.com"),
+		"cn-shenzhen-su18-b01":        dara.String("cs.aliyuncs.com"),
+		"cn-wuhan":                    dara.String("cs.aliyuncs.com"),
+		"cn-yushanfang":               dara.String("cs.aliyuncs.com"),
+		"cn-zhangbei":                 dara.String("cs.aliyuncs.com"),
+		"cn-zhangbei-na61-b01":        dara.String("cs.aliyuncs.com"),
+		"cn-zhangjiakou-na62-a01":     dara.String("cs.aliyuncs.com"),
+		"cn-zhengzhou-nebula-1":       dara.String("cs.aliyuncs.com"),
+		"eu-west-1-oxs":               dara.String("cs.aliyuncs.com"),
+		"rus-west-1-pop":              dara.String("cs.aliyuncs.com"),
+	}
+	err = client.CheckConfig(config)
+	if err != nil {
+		return err
+	}
+	client.Endpoint, err = client.GetEndpoint(dara.String("cs"), client.RegionId, client.EndpointRule, client.Network, client.Suffix, client.EndpointMap, client.Endpoint)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (client *Client) GetEndpoint(productId *string, regionId *string, endpointRule *string, network *string, suffix *string, endpointMap map[string]*string, endpoint *string) (result *string, err error) {
+	if !dara.IsNil(endpoint) {
+		result = endpoint
+		return result, err
+	}
+
+	if !dara.IsNil(endpointMap) && !dara.IsNil(endpointMap[dara.StringValue(regionId)]) {
+		result = endpointMap[dara.StringValue(regionId)]
+		return result, err
+	}
+
+	body, err := openapiutil.GetEndpointRules(productId, regionId, endpointRule, network, suffix)
+	if err != nil {
+		return result, err
+	}
+	result = body
+	return result, err
+}
